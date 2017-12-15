64e175329d8aee2289a51908812a8207e3016dfd
clitetailor <ducnhatphung@gmail.com>
2017-12-16 00:32:49 +0700

    solved: quicksort

--- Lab002/app/QuickSort.hs
+++ Lab002/app/QuickSort.hs
@@ -1,5 +1,25 @@
 module Main where
 
+import System.Environment (getArgs)
+import Data.List (foldl')
+
+quicksort :: (Ord a) => [a] -> [a]  
+quicksort [] = []  
+quicksort (x:xs) =   
+    let smallerSorted = quicksort [a | a <- xs, a <= x]  
+        biggerSorted = quicksort [a | a <- xs, a > x]  
+    in  smallerSorted ++ [x] ++ biggerSorted  
+
+addStr :: String -> Int -> String
+addStr accum num =
+    accum ++ (show num) ++ " "
+
+toInt :: String -> Int
+toInt i0 = read i0
+
 main :: IO ()
 main = do
-    putStrLn "Hello, world!"
+    list <- getArgs
+    let sortedList = quicksort (fmap toInt list)
+    let str = foldl' addStr "" sortedList
+    putStr str
