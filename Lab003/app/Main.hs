05e1a0c4a37b571892bbdc916c25e1937eeb3fe5
clitetailor <ducnhatphung@gmail.com>
2017-12-17 17:44:39 +0700

    testing with binary-tree

--- Lab003/app/Main.hs
+++ Lab003/app/Main.hs
@@ -2,5 +2,24 @@ module Main where
 
 import Lib
 
+data Node = Tree { left :: Node, right :: Node }
+        |   Leaf { value :: Int }
+        deriving (Show)
+
 main :: IO ()
-main = someFunc
+main = do
+    let node = Tree {
+        left = Leaf {
+            value = 1
+        },
+        right = Leaf {
+            value = 2
+        }
+    }
+    let newNode = increase node
+    putStrLn $ show node
+    putStrLn $ show newNode
+    where
+        increase n0 =
+            case n0 of  Tree _ _ -> n0 { left = increase $ left n0 }
+                        Leaf _ -> n0 { value = (value n0) + 1 }
