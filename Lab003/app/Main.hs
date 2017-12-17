0923306ae2bb2c54f156a060a5565e88311eda7c
clitetailor <ducnhatphung@gmail.com>
2017-12-17 20:47:28 +0700

    feat: normal checkout-system

--- Lab003/app/Main.hs
+++ Lab003/app/Main.hs
@@ -1,25 +1,49 @@
 module Main where
 
-import Lib
+import Data.List (length, foldl')
+import Data.Eq (Eq)
 
-data Node = Tree { left :: Node, right :: Node }
-        |   Leaf { value :: Int }
-        deriving (Show)
+data Fruit = Apple | Orange deriving (Show)
+
+data Info = Info { _fruits :: [Fruit],
+                   _cost :: Int } deriving (Show)
+
+data Offer = Nil | OfferOne | OfferTwo
+data CheckoutSystem = CheckoutSystem { offer :: Offer }
+
+
+instance Eq Fruit where
+    Apple == Apple = True
+    Orange == Orange = True
+    _ == _ = False
+
+
+fruits :: CheckoutSystem -> [Fruit] -> [Fruit]
+fruits sys order =
+    case offer sys of   Nil -> order
+                        OfferOne -> []
+                        OfferTwo -> []
+
+
+totalCost :: CheckoutSystem -> [Fruit] -> Int
+totalCost (CheckoutSystem Nil) fruits =
+    foldl' sumUp 0 [Apple, Orange]
+    where
+        sumUp accum fruit = accum + (fruitPrice fruit) * (nFruit fruit)
+        nFruit fruit = length [f1 | f1 <- fruits, f1 == fruit]
+
+
+checkout :: CheckoutSystem -> [Fruit] -> Info
+checkout sys f0 = Info { _fruits = fruits sys f0,
+                         _cost = totalCost sys f0 }
+
+
+fruitPrice :: Fruit -> Int
+fruitPrice Apple = 60
+fruitPrice Orange = 25
 
 main :: IO ()
 main = do
-    let node = Tree {
-        left = Leaf {
-            value = 1
-        },
-        right = Leaf {
-            value = 2
-        }
-    }
-    let newNode = increase node
-    putStrLn $ show node
-    putStrLn $ show newNode
-    where
-        increase n0 =
-            case n0 of  Tree _ _ -> n0 { left = increase $ left n0 }
-                        Leaf _ -> n0 { value = (value n0) + 1 }
+    let sys = CheckoutSystem Nil
+    let info = checkout sys [Apple, Apple, Orange]
+    putStr $ show info
